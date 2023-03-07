655fe2ddafa968c544f505bb170d1e17daa8151e
EMMANUEL-RONOH <roohemmanuel2@gmail.com>
2023-03-07 18:29:05 +0300

    initial commit

---
+++ Common/footer.php
@@ -0,0 +1,27 @@
+<footer>
+  <div class="footer-top">
+    <div class="container">
+        <div class="row">
+          <div class="col-md-4 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>DIPLOMA PROGRAMS</h2>
+            <ul>
+              <li><a href="">Dip Civil Engineering</a></li>
+              <li><a href="">Dip. Electrical Engineering</a></li>
+              <li><a href="">Dip Mechanical Engineering</a></li>
+              <li><a href="">Dip Architecture</a></li>
+              <li><a href="">Dip Information Technology</a></li>
+              <li><a href="">Dip Business Administration</a></li>
+            </ul>
+          </div>  
+          <div class="col-md-3 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>CONTACT DETAILS</h2>
+              <p>Address: Merigi,
+                  Bomet – KENYA</p>
+              <p>Telephones: +254711209721</p>
+              <p>E-mail: <a href="mailto:" >ronohemmanuel2@gmail.com</p>
+        </div>
+      </div>
+    </div>
+    </div>
+  <p class="footer-bottom-text"> CopyRights © Alright Reserve 2022</p>
+</footer>
---
+++ academics.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Academics</title>
+</head>
+<body>
+	<?php include('common/header.php') ?>
+	<div class="container-fluid">
+		<div class="row mb-5">
+			<div class="academic-area-headings">
+				<h4 class="py-4">Faculty of Engineering and Technology</h4>
+			</div>
+			<div class="container">
+				<div class="row">
+		
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="row mb-5">
+			<div class="academic-area-headings">
+				<h4 class="py-4">ICT Department</h4>
+			</div>
+			<div class="container">
+				<div class="row">
+					
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="row mb-5">
+			<div class="academic-area-headings">
+				<h4 class="py-4">Electrical/Electronics Department</h4>
+			</div>
+			<div class="container">
+				<div class="row">
+					
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="row mb-5">
+			<div class="academic-area-headings">
+				<h4 class="py-4">Automotive Department</h4>
+			</div>
+			<div class="container">
+				<div class="row">
+						
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="row mb-5">
+			<div class="academic-area-headings">
+				<h4 class="py-4">Building & Civil Engineering Department</h4>
+			</div>
+			<div class="container">
+				<div class="row">
+					
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="row mb-5">
+			<div class="academic-area-headings">
+				<h4 class="py-4">Applied Science Department</h4>
+			</div>
+			<div class="container">
+				<div class="row">
+					
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="row mb-5">
+			<div class="academic-area-headings">
+				<h4 class="py-4">Business Studies Department</h4>
+			</div>
+			<div class="container">
+				<div class="row">
+					
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
